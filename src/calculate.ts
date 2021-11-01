c09506b0b5742c89369b94861934e1b58f9d78d9
deman4ela <dimatopoli@gmail.com>
2021-11-01 22:35:51 +0200

    * and / implementation

---
+++ src/calculate.ts
@@ -0,0 +1,29 @@
+export class Calculate {
+
+    protected _result: number
+    
+    constructor(){
+        this._result = 0
+    }
+  
+    get result(){
+      return this._result
+    }
+
+    public plus(numberFirst: number, numberSecond: number): void{
+      this._result = numberFirst + numberSecond
+    }
+  
+    public minus(numberFirst: number, numberSecond: number): void{
+      this._result = numberFirst - numberSecond
+    }
+
+    public multiply(numberFirst: number, numberSecond: number): void{
+        this._result = numberFirst * numberSecond
+      }
+    
+      public divide(numberFirst: number, numberSecond: number): void{
+        this._result = numberFirst / numberSecond
+      }
+      
+}
